72b2c5c717e6227c4e9e0284a7a1127f8217f6ee
Pratik Borole <borolepratik@gmail.com>
2021-06-17 20:50:59 -0700

    feat: set up the base for further work

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+let timeOrAngle = 0;
+
 function setup() {
   createCanvas(800, 600);
 }
 
 function draw() {
   background(0);
+
+  // move viewport away from screen edge
+  translate(200, 200);
+  let radius = 100;
+
+  stroke(255);
+  noFill();
+  // create empty main circle with diameter
+  ellipse(0, 0, radius * 2);
+
+  // instantaneous position for a point on main circle
+  // calculated based on `timeOrAngle`
+  // aka polar to cartesian transformation
+  let x = radius * cos(timeOrAngle);
+  let y = radius * sin(timeOrAngle);
+
+  // draw a line from main circle center to the point(x,y)
+  line(0, 0, x, y);
+
+  timeOrAngle += 0.01;
 }
